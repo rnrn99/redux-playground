7ec5ab2f59e936af96b34192a1788af3e8239fe5
rnrn99 <giu0213@naver.com>
2022-10-24 14:28:50 +0900

    [Add] add asynchronously without middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import type { NextPage } from "next";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
-import { increment, decrement } from "./redux/counterSlice";
+import { increment, decrement, incrementByAmount } from "./redux/counterSlice";
+
+type ResponseValue = {
+  sumValue: number;
+};
 
 const Home: NextPage = () => {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const addAsync = async () => {
+    const res = await fetch("/api/sum");
+    const { sumValue }: ResponseValue = await res.json();
+    dispatch(incrementByAmount(sumValue));
+  };
 
   return (
     <>
       <div>{count}</div>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={addAsync}>add asynchronously</button>
     </>
   );
 };
